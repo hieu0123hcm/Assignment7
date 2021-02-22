191937c8e1e56174089f1bb6c41cb1b539e117e8
hieu0123hcm <43198687+hieu0123hcm@users.noreply.github.com>
2021-02-22 12:59:25 +0700

    Start

---
+++ TuitionFeesApp/src/main/java/com/TuitionFeesApp/TuitionFeesAppApplication.java
@@ -0,0 +1,13 @@
+package com.TuitionFeesApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuitionFeesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuitionFeesAppApplication.class, args);
+	}
+
+}
---
+++ TuitionFeesApp/src/test/java/com/TuitionFeesApp/TuitionFeesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TuitionFeesApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuitionFeesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
